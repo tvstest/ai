650767fca5cc1a544d1b545fa6ca16ebad1d069c
Rahul Parmar <rahul.parmar@tatvasoft.com>
2021-09-01 17:56:44 +0530

    revised design

--- country-flag/src/app/components/Country/CountryCard/index.tsx
+++ country-flag/src/app/components/Country/CountryCard/index.tsx
@@ -5,8 +5,9 @@ import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import CardActionArea from '@material-ui/core/CardActionArea'
-import { CardMedia } from '@material-ui/core'
+import CardMedia from '@material-ui/core/CardMedia'
 import { ICountry } from 'app/utils/interfaces/country'
+import Divider from '@material-ui/core/Divider'
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +18,15 @@ const useStyles = makeStyles({
   },
   media: {
     height: 140,
+  },
+  centered: {
+    justifyContent: 'center',
+  },
+  customCard: {
+    display: 'flex',
+    height: 184,
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 })
 
@@ -38,23 +48,30 @@ const CountryCard = ({
           image={country.flag}
           title={country.name}
         />
-        <CardContent>
+        <Divider />
+        <CardContent className={classes.customCard}>
           <Typography gutterBottom variant="h6">
             {country.name}
             <Typography variant="subtitle1" component="p" color="textSecondary">
               {country.capital}
             </Typography>
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Population : {country.population}
+          <Typography variant="body1" color="primary" component="p">
+            Population :{' '}
+            <Typography variant="body2" component="span" color="textSecondary">
+              {country.population}
+            </Typography>
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body1" color="primary" component="p">
             Latitude, Longitude :{' '}
-            {country.latlng[0] + '°  ' + country.latlng[1] + '°'}
+            <Typography variant="body2" component="span" color="textSecondary">
+              {country.latlng[0] + '°  ' + country.latlng[1] + '°'}
+            </Typography>
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <Divider />
+      <CardActions className={classes.centered}>
         <Button
           onClick={onClickWeatherCapitalButton}
           variant="contained"
