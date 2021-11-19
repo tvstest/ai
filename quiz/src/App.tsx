25b9e2914c61e540bdf6c14aa64de676295226fd
Yash Sonani <yash.sonani@tatvasoft.com>
2021-11-19 15:08:38 +0530

    stepper component

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,23 +1,33 @@
-import logo from './logo.svg'
 import './App.css'
+import Stepper, { IStep } from './components/Stepper'
+
+const steps: IStep[] = [
+  {
+    step: 1,
+    status: 'answered',
+  },
+  {
+    step: 2,
+    status: 'not_answered',
+  },
+  {
+    step: 3,
+    status: 'not_answered',
+  },
+  {
+    step: 4,
+    status: 'answered',
+  },
+  {
+    step: 5,
+    status: 'answered',
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ marginTop: 10 }}>
+      <Stepper steps={steps} onClick={(e) => console.log(e)} />
     </div>
   )
 }
