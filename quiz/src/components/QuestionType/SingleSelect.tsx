63a0e0df3c06561cc51ef6c85ba624eb0606400e
Yash Sonani <yash.sonani@tatvasoft.com>
2021-11-23 14:43:48 +0530

    edit functionality component working

--- quiz/src/components/QuestionType/SingleSelect.tsx
+++ quiz/src/components/QuestionType/SingleSelect.tsx
@@ -14,10 +14,12 @@ const SingleSelect: React.FC<ISingleSelectProps> = ({
   handleAnswer,
   questionsData,
 }) => {
-  const [checked, setChecked] = useState([])
+  const [checked, setChecked] = useState(
+    (questionsData.userAnswer as Array<number>) ?? []
+  )
 
   const singleSelectHandler = (value: string) => {
-    setChecked([value])
+    setChecked([Number(value)])
     handleAnswer([Number(value)])
   }
 
