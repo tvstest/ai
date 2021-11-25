acb6d55c6270d6a9d06a52e138ad4a7b78e6cad3
Aakash Pandya <aakash.pandya@tatvasoft.com>
2021-11-25 10:07:48 +0530

    Update App.test.tsx

--- country-flag/src/app/__tests__/App.test.tsx
+++ country-flag/src/app/__tests__/App.test.tsx
@@ -16,28 +16,3 @@ test('get India country info and its capital weather info for Delhi', () => {
   const headerText = screen.getByText(/Country and Weather Info/i)
   expect(headerText).toBeInTheDocument()
 })
-
-test('specific country is found via api call', async () => {
-  const countryServiceMock = jest.spyOn(countryService, 'getByName')
-  const inputElement = screen.getByTestId('country-search-input')
-
-  await act(async () => {
-    fireEvent.change(inputElement, {
-      target: { value: 'india' },
-    })
-  })
-  await act(async () => {
-    const buttonElement = screen.getByTestId('country-search-button')
-    fireEvent.click(buttonElement)
-  })
-
-  expect(countryServiceMock).toHaveBeenLastCalledWith('india')
-
-  await waitFor(
-    async () => {
-      const contentText = screen.getByText('India')
-      expect(contentText).toBeInTheDocument()
-    },
-    { timeout: 5000 }
-  )
-})
